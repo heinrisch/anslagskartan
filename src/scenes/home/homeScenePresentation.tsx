b09da1ecef3934fe88bf5967bb76cf5d9c67a271
Misse <misse@mis.se>
2020-04-03 22:17:56 +0200

    only use leaflet map for now

--- src/scenes/home/homeScenePresentation.tsx
+++ src/scenes/home/homeScenePresentation.tsx
@@ -1,20 +1,12 @@
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
 import { Map } from "../../components/map/map";
-import GMap from "../../Map";
 
 export const HomeScenePresentation: React.FC = memo(() => {
-  const [useGMaps, setUseGMaps] = useState(true);
-
-  const renderedMap = useGMaps ? (
-    <GMap />
-  ) : (
-    <Map defaultCenter={{  lat: 59.310519, lng: 18.057875 }} defaultZoom={13} />
-  );
-
   return (
-    <>
-      <button onClick={() => setUseGMaps(!useGMaps)}>Switch Map</button>
-      {renderedMap}
-    </>
+    <Map
+      defaultCenter={{ latitude: 59.310519, longitude: 18.057875 }}
+      defaultZoom={13}
+      markers={[]}
+    />
   );
 });
