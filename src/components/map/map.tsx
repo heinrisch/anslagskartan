1762779a9661930a1b0246bde78f550509734478
Misse <misse@mis.se>
2020-04-03 20:26:39 +0200

    add differente tile styles to leaflet

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { Map as LeafMap, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 
@@ -12,28 +12,61 @@ type MapProps = {
 
 export const Map: React.FC<MapProps> = memo((props) => {
   const { defaultCenter, defaultZoom } = props;
+  const [tileType, setTileType] = useState(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  );
+
+  const handleChange = (event: any) => {
+    setTileType(event.target.value);
+  };
 
   return (
-    <LeafMap
-      center={[defaultCenter.lat, defaultCenter.lng]}
-      zoom={defaultZoom}
-      style={{ width: "100%", height: "100vh" }}
-    >
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <MarkerClusterGroup>
-        <Marker position={[defaultCenter.lat, defaultCenter.lng]}>
-          <Popup>1</Popup>
-        </Marker>
-        <Marker position={[defaultCenter.lat - 1, defaultCenter.lng - 1]}>
-          <Popup>2</Popup>
-        </Marker>
-        <Marker position={[defaultCenter.lat + 1, defaultCenter.lng + 1]}>
-          <Popup>3</Popup>
-        </Marker>
-      </MarkerClusterGroup>
-    </LeafMap>
+    <>
+      <select onChange={handleChange}>
+        <option value="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
+          default
+        </option>
+        <option value="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png">
+          alidade
+        </option>
+        <option value="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png">
+          osm
+        </option>
+        <option value="https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png">
+          hydda
+        </option>
+        <option value="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}">
+          worldStreet
+        </option>
+        <option value="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}">
+          worldTopo
+        </option>
+        <option value="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png">
+          voyager
+        </option>
+      </select>
+
+      <LeafMap
+        center={[defaultCenter.lat, defaultCenter.lng]}
+        zoom={defaultZoom}
+        style={{ width: "100%", height: "100vh" }}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url={tileType}
+        />
+        <MarkerClusterGroup>
+          <Marker position={[defaultCenter.lat, defaultCenter.lng]}>
+            <Popup>1</Popup>
+          </Marker>
+          <Marker position={[defaultCenter.lat - 1, defaultCenter.lng - 1]}>
+            <Popup>2</Popup>
+          </Marker>
+          <Marker position={[defaultCenter.lat + 1, defaultCenter.lng + 1]}>
+            <Popup>3</Popup>
+          </Marker>
+        </MarkerClusterGroup>
+      </LeafMap>
+    </>
   );
 });
