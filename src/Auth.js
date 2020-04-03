fce6e7a2c2bb9afdadf0c7b5072fad3a22601e89
Henrik Sandström <sandstrom.henrik@gmail.com>
2020-04-03 21:56:41 +0200

    auth with facebook

---
+++ src/Auth.js
@@ -0,0 +1,9 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+    apiKey: "AIzaSyDSVpAgrUGOtfXydlsEnlDKSES8XFwDEsM",
+    projectId: "billboard-map-273108",
+};
+
+
