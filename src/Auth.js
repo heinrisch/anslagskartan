51b07aed185e5786f4a0b1ae45e8407636b59ab6
Misse <misse@mis.se>
2020-04-04 00:03:16 +0200

    add font awesome and remove files not un use, also move google map files

--- src/Auth.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-
-const config = {
-    apiKey: "AIzaSyDSVpAgrUGOtfXydlsEnlDKSES8XFwDEsM",
-    projectId: "billboard-map-273108",
-};
-
-
