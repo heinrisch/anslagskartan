24920f908c5dbebd6c67efc616e73c1bc17af7a1
Henrik Sandström <sandstrom.henrik@gmail.com>
2020-04-05 21:07:52 +0200

    update firebase config

--- web/src/utils/firebaseConfig.ts
+++ web/src/utils/firebaseConfig.ts
@@ -1,6 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBPe0V7D76NBcJSnjQrKc8XZ-ZHlh-coB4",
-    projectId: "billboard-map-273108",
-    databaseURL: "DATABASE_URL",
+    apiKey: "AIzaSyCgNroUcohTYmu05XUgLS02RnH79QUlEHs",
     authDomain: "billboard-map-273108.firebaseapp.com",
+    databaseURL: "https://billboard-map-273108.firebaseio.com",
+    projectId: "billboard-map-273108",
+    storageBucket: "billboard-map-273108.appspot.com",
+    messagingSenderId: "40115610522",
+    appId: "1:40115610522:web:7e23526425256a60c71023",
+    measurementId: "G-9DVNV4LHZR"
 };
